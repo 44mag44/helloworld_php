2fa7ef859069086a277b81988b12aea83ae028fd
44mag44 <antistop91@mail.ru>
2021-01-25 21:54:19 +0900

    -

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
-
+<html>
+	<head>
+		<title>less</title>
+	</head>
+<body>
 	<?php
 		"Hello World! PHP";
 	?>
-
+	<p>Заголовок 1</p>
+	<blockquote>"Test server Github"</blockquote>
+</body>
+</html>
