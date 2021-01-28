e356575c5a61c161a475ecaa680bae9a1729041b
44mag44 <antistop91@mail.ru>
2021-01-29 01:40:12 +0900

    -

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
  <!DOCTYPE html>
 <html>
-	<body>
-		<?php
-			$username = "Petr Gusev";
-			echo $username;
-			echo "<br>"
-			$current_user = $mynameis;
-			echo $current_user;
-		?>
-	</body>
+<body>
+	<?php
+		$username = "Petr Gusev";
+		echo $username;
+		echo "<br>"
+		$current_user = $mynameis;
+		echo $current_user;
+	?>
+</body>
 </html> 
-
